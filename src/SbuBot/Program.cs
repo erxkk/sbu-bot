55da9045d2422b6758cfa7ea0c269e9bc55937d2
erxkk <ErikBuennig@gmail.com>
2021-05-19 20:48:41 +0200

    Initial Commit

---
+++ src/SbuBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SbuBot
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
