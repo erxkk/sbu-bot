97970eec45d06ac64e61adab7b0e5684f1951837
erxkk <ErikBuennig@gmail.com>
2021-10-28 21:02:09 +0200

    added role more hierarchy checks

--- src/SbuBot/Commands/Modules/ColorRoleModule.Claim.cs
+++ src/SbuBot/Commands/Modules/ColorRoleModule.Claim.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using Disqord;
 using Disqord.Bot;
+using Disqord.Gateway;
 using Disqord.Rest;
 
 using Qmmands;
@@ -16,7 +18,6 @@ namespace SbuBot.Commands.Modules
 {
     public sealed partial class ColorRoleModule
     {
-        // TODO: overhaul hierarchy check, pretty sure this will not work
         [Group("claim")]
         [RequireColorRole(false)]
         [Description("Claims the given color role if it has no owner.")]
@@ -25,12 +26,14 @@ namespace SbuBot.Commands.Modules
             [Command]
             [Usage("role claim some role name", "r take 732234804384366602", "r claim @SBU-Bot")]
             public async Task<DiscordCommandResult> ClaimAsync(
-                [RequireHigherHierarchy, MustBeOwned(false)]
-                [Description("The role to claim.")]
-                [Remarks("Must be a color role.")]
+                [MustBeOwned(false)][Description("The role to claim.")][Remarks("Must be a color role.")]
                 SbuColorRole role
             )
             {
+                // we ignore possible null values and just say it's higher lol
+                if (Context.CurrentMember.GetHierarchy() <= Context.Guild.Roles.GetValueOrDefault(role.Id)?.Position)
+                    await Reply("The role is higher than mine I can't assign it.");
+
                 await Context.Author.GrantRoleAsync(role.Id);
 
                 var context = Context.GetSbuDbContext();
@@ -45,12 +48,15 @@ namespace SbuBot.Commands.Modules
             [Command]
             [Usage("role claim some role name", "r take 732234804384366602", "r claim @SBU-Bot")]
             public async Task<DiscordCommandResult> ClaimNewAsync(
-                [MustBeColorRole, RequireHigherHierarchy, MustExistInDb(false)]
+                [MustBeColorRole, MustExistInDb(false)]
                 [Description("The role to claim")]
                 [Remarks("Must be a color role.")]
                 IRole role
             )
             {
+                if (Context.CurrentMember.GetHierarchy() <= role.Position)
+                    await Reply("The role is higher than mine I can't assign it.");
+
                 await Context.Author.GrantRoleAsync(role.Id);
 
                 SbuDbContext context = Context.GetSbuDbContext();
