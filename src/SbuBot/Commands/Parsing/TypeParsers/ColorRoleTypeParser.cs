9c44ef5856e54e153ccc9f62f0a3e19bb29bf358
erxkk <ErikBuennig@gmail.com>
2021-10-28 18:56:31 +0200

    fixed color role parser

--- src/SbuBot/Commands/Parsing/TypeParsers/ColorRoleTypeParser.cs
+++ src/SbuBot/Commands/Parsing/TypeParsers/ColorRoleTypeParser.cs
@@ -21,13 +21,8 @@ namespace SbuBot.Commands.Parsing.TypeParsers
             SbuColorRole? role = null;
             TypeParser<IRole> roleParser = context.Bot.Commands.GetTypeParser<IRole>();
 
-            if (await roleParser.ParseAsync(parameter, value, context) is { IsSuccessful: true } roleParseResult
-                && roleParseResult.Value.Position < context.Bot.GetColorRoleSeparator().Position
-                && roleParseResult.Value.Color is { }
-            )
-            {
+            if (await roleParser.ParseAsync(parameter, value, context) is { IsSuccessful: true } roleParseResult)
                 role = await context.GetColorRoleAsync(roleParseResult.Value);
-            }
 
             return role is { }
                 ? Success(role)
