819feb727662321d0be62d9cafb87a0f7450cc9a
erxkk <ErikBuennig@gmail.com>
2021-10-30 17:08:14 +0200

    refactored docker migration script

--- docker/migrate.sh
+++ docker/migrate.sh
@@ -2,18 +2,20 @@
 
 # quick exit with error message
 bail() {
-    if  [[ -e 'tmp.env' ]]; then
-        echo "==> Removing 'tmp.env'"
-        rm 'tmp.env'
-    fi
     echo "###" $1
     exit 1;
 }
 
+_ENV=".env"
+
 # move to dir if passed
-if [[ ! -z $1 ]]; then
-    echo "==> changing dir to '$1'"
-    cd $1 || bail "Could not cd to $1: $(cd $1)"
+if [[! -z $1 ]]; then
+    if [[ -e $1 ]]; then
+        echo "===> Using .env file '$1'"
+        _ENV=$1
+    else
+        bail "File does not exist '$1'"
+    fi
 fi
 
 # declare docker names
@@ -25,35 +27,25 @@ _SBU_DATABASE='postgres'
 
 # check if volume exists
 if [[ -z "$(docker volume ls | grep $_SBU_VOLUME)" ]]; then
-    echo "==> Creating volume '$_SBU_VOLUME'"
+    echo "===> Creating volume '$_SBU_VOLUME'"
     docker volume create $_SBU_VOLUME || bail "Could not create $_SBU_VOLUME"
 fi
 _VOLUME_MOUNT=$(docker volume inspect -f '{{.Mountpoint}}' $_SBU_VOLUME)
-echo "==> Found volume mount '$_VOLUME_MOUNT'"
+echo "===> Found volume mount '$_VOLUME_MOUNT'"
 
 # check if db is running
 if [[ -z "$(docker ps | grep $_SBU_DATABASE)" ]]; then
-    bail "==> Could not find db $_SBU_DATABASE"
+    bail "===> Could not find db $_SBU_DATABASE"
 fi
 
 # check if network exists
 if [[ -z "$(docker network ls | grep $_SBU_NETWORK)" ]]; then
-    echo "==> Creating network '$_SBU_NETWORK'"
+    echo "===> Creating network '$_SBU_NETWORK'"
     docker network create $_SBU_NETWORK || bail "Could not create $_SBU_NETWORK"
 
-    echo "==> Connecting '$_SBU_DATABASE' to '$_SBU_NETWORK'"
+    echo "===> Connecting '$_SBU_DATABASE' to '$_SBU_NETWORK'"
     docker network connect $_SBU_NETWORK $_SBU_DATABASE || bail "Could not connect $_SBU_DATABASE to $_SBU_NETWORK"
 fi
-_POSTGRES_HOST_IP=$(docker network inspect -f '{{range.Containers}}{{.Name}} {{.IPv4Address}}{{end}}' $_SBU_NETWORK \
-    | grep $_SBU_DATABASE | grep -Po '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-echo "==> Found postgres network ip '$_POSTGRES_HOST_IP'"
-
-# make tmp.env
-echo "===> Creating 'tmp.env'"
-cp '.env' 'tmp.env' || bail "Could not copy .env => tmp.env"
-
-# write ip for host to env file
-echo "BOT_POSTGRES__HOST=$_POSTGRES_HOST_IP" >> 'tmp.env'
 
 # if container exists stop and remove
 if [[ ! -z "$(docker ps -a | grep $_SBU_CONTAINER)" ]]; then
@@ -74,11 +66,9 @@ fi
 echo "===> Starting $SBU_BOT_CONTAINER"
 docker run -d \
     --name $_SBU_CONTAINER \
-    --env-file 'tmp.env' \
+    --env-file $_ENV \
     --volume $_SBU_VOLUME:/volume \
     --network $_SBU_NETWORK \
     $_SBU_IMAGE \
         || bail "Could not run $_SBU_CONTAINER"
 
-echo "===> Removing 'tmp.env'"
-rm 'tmp.env'
